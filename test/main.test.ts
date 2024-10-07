49c9af39209feeb35fc0e6003a2cb76fe90f3665
KithLo <26156452+KithLo@users.noreply.github.com>
2024-10-07 10:26:09 +0800

    Fix test cases

--- test/main.test.ts
+++ test/main.test.ts
@@ -2,16 +2,16 @@ import { maxGrade } from "../src/grades"
 import { Institution } from "../src/institutions"
 import { programmes } from "../src/programmes"
 import { Subject } from "../src/subjects"
-import { SubjectScores } from "../src/types"
+import { SubjectGrades } from "../src/types"
 
-const testResults: SubjectScores[] = [
+const testResults: SubjectGrades[] = [
     {
-        [Subject.Chi]: 5,
-        [Subject.Eng]: 5,
-        [Subject.Maths]: 5,
-        [Subject.CS]: 1,
-        [Subject.Phys]: 5,
-        [Subject.Chem]: 5,
+        [Subject.Chi]: "5",
+        [Subject.Eng]: "5",
+        [Subject.Maths]: "5",
+        [Subject.CS]: "A",
+        [Subject.Phys]: "5",
+        [Subject.Chem]: "5",
     },
     maxGrade,
 ]
@@ -20,15 +20,27 @@ function* iterate() {
     for (const institution in programmes) {
         for (const programme of programmes[institution as Institution]) {
             for (const result of testResults) {
-                yield [result, programme] as const
+                yield [programme.mapGrades(result), programme] as const
             }
         }
     }
 }
 
 describe("Test all programmes requirement and weightings", () => {
+    const list = Array.from(iterate())
+
+    test("duplicate", () => {
+        const visited = new Set<string>()
+        for (const institution in programmes) {
+            for (const programme of programmes[institution as Institution]) {
+                expect(visited).not.toContain(programme.id)
+                visited.add(programme.id)
+            }
+        }
+    })
+
     test("requirements", () => {
-        for (const [result, programme] of iterate()) {
+        for (const [result, programme] of list) {
             const output = programme.requirement(result)
             if (output !== null) {
                 expect(output).toEqual(expect.any(Object))
@@ -36,7 +48,7 @@ describe("Test all programmes requirement and weightings", () => {
         }
     })
     test("weightings", () => {
-        for (const [result, programme] of iterate()) {
+        for (const [result, programme] of list) {
             const output = programme.weighting(result)
             if (output !== null) {
                 expect(output).toEqual(expect.any(Object))
